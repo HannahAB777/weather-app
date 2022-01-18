3d72c33e939d5a69c6c475e37dbb94165f44ece0
hannah AB <hannah.allisonbatt@gmail.com>
2022-01-18 11:39:28 +0800

    draft outline

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+//time input
+
+// fetch for data from weather sight
+//with a input variable for location
+
+//display data in created elements
+//create options for each of the variables
+
+//search options with 
+
+
+
+//a colour loop for 
