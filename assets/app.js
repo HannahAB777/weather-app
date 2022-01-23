1868a63fca89f687e91eb0798493ffc230b06cd6
hannah AB <hannah.allisonbatt@gmail.com>
2022-01-23 16:08:33 +0800

    working forcast + UV

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,17 @@
-let button = document.getElementById("button");
-let city = document.getElementById("city");
-let temp = document.getElementById("temp");
-let humidity = document.getElementById("humidity");
-let wind = document.getElementById("wind");
-let UV = document.getElementById("UV");
-let todayIcon = document.getElementById("icon");
-const dayOne = document.getElementById("day-one")
-const dayTwo = document.getElementById("day-two");
-const dayThree = document.getElementById("day-three");
-const dayFour = document.getElementById("day-four");
-const dayFive = document.getElementById("day-five");
-const dayContainer = document.getElementsByClassName("days");
+const button = document.getElementById("button");
+const city = document.getElementById("city");
+const temp = document.getElementById("temp");
+const humidity = document.getElementById("humidity");
+const wind = document.getElementById("wind");
+const UV = document.getElementById("UV");
+const todayIcon = document.getElementById("icon");
+const dayContainer = document.getElementById("five-day-forecast");
 
 
 button.addEventListener("click", function (event) {
   event.preventDefault();
-  let placeInput = document.getElementById("search");
-  let place = placeInput.value;
+  const placeInput = document.getElementById("search");
+  const place = placeInput.value;
   const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + place + '&appid=8036ed1d4d3026cb916b26417cd7e2c8';
 
   fetch(weatherUrl)
@@ -24,63 +19,56 @@ button.addEventListener("click", function (event) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
       city.textContent = "City: " + data.name;
       temp.textContent = "Temperature: " + (Math.floor(data.main.temp - 273.15)) + "°C";
       humidity.textContent = "Humidity: " + data.main.humidity;
       wind.textContent = "Wind: " + data.wind.speed + "km/h";
       todayIcon.src = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-      let todayLat = data.coord.lat;
-      let todayLon = data.coord.lon;
-      let UVToday = "https://api.openweathermap.org/data/2.5/onecall?lat=" + todayLat + "&lon=" + todayLon + "&appid=8036ed1d4d3026cb916b26417cd7e2c8";
+      const todayLat = data.coord.lat;
+      const todayLon = data.coord.lon;
+      const UVToday = "https://api.openweathermap.org/data/2.5/onecall?lat=" + todayLat + "&lon=" + todayLon + "&appid=8036ed1d4d3026cb916b26417cd7e2c8";
       return fetch(UVToday)
         .then(function (response) {
           return response.json();
         });
-    }).then(function (data) {
-      console.log(data);
-      UV.textContent = "UV-Index: " + data.current.uvi;
-    });
-
-  futureForecast();
-});
-
-
-
-function futureForecast() {
-  let placeInput = document.getElementById("search");
-  let place = placeInput.value;
-  const fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + place + "&appid=8036ed1d4d3026cb916b26417cd7e2c8";
-
-  return fetch(fiveDayForecast)
-    .then(function (response) {
-      return response.json();
-    }).then(function (weekdata) {
-      console.log(weekdata);
-
-      const fiveDayForecast = weekdata.daily.slice(0,5);
-
-      for (let i = 0; i < fiveDayForecast.length; i++) {
-        const weather = fiveDayForecast[i];
-
-        const col = createCards(weather.dt, '', weather.temp.day, weather.humidity, weather.wind_speed);
-
-        dayContainer.appendChild(col);
-
-        
+    }).then(function (oneCallData) {
+      UV.textContent = "UV-Index: " + Math.floor(oneCallData.current.uvi);
+      const UVdata = Math.floor(oneCallData.current.uvi);
+      if (UVdata < 3){
+        UV.classList.add("favourable");
       }
-
+      if (UVdata >= 3 && UVdata <= 5){
+        UV.classList.add("moderate");
+      }
+      if(UVdata >5 && UVdata <= 8){
+        UV.classList.add("high");
+      }
+      if(UVdata >= 9){
+        UV.classList.add("severe");
+      }
+      console.log(oneCallData);
+      dayContainer.textContent= "";
+      const forecast = oneCallData.daily.slice(1,6);
+      
+          for (let i = 0; i < forecast.length; i++) {
+            const weather = forecast[i];
+            
+            const card = createCards(weather.dt, weather.weather[0].icon, weather.temp.day, weather.humidity, weather.wind_speed);
+      
+            dayContainer.appendChild(card);
+          }
     });
-    
-    function createCards(date, icon, temp, humidity, wind) {
+
+});
+function createCards(date, icon, temp, humidity, wind) {
   
   
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
-      dayContainer.appendChild(card);
+      card.setAttribute('class', 'col');
   
       const iconEl = document.createElement("img");
-      iconEl.setAttribute("src", icon);
+      iconEl.setAttribute("src", "https://openweathermap.org/img/w/"+ icon + ".png");
       card.appendChild(iconEl);
   
       const weathercontainer = document.createElement('div');
@@ -90,7 +78,7 @@ function futureForecast() {
       const dateheader = document.createElement("h4");
       dateheader.setAttribute("class", "card-title");
       weathercontainer.appendChild(dateheader);
-      dateheader.textContent = date;
+      dateheader.textContent = moment.unix(date).format("dddd");
   
       const p = document.createElement('p');
       weathercontainer.appendChild(p);
@@ -100,19 +88,19 @@ function futureForecast() {
   
       const tempLi = document.createElement('li');
       ul.appendChild(tempLi);
-      tempLi.textContent = temp;
+      tempLi.textContent = "Temperature: " + (Math.floor(temp - 273.15)) + "°C";
   
       const humidityEl = document.createElement('li');
       ul.appendChild(humidityEl);
-      humidityEl.textContent = humidity;
+      humidityEl.textContent = "Humidity: " + humidity;
   
       const windEl = document.createElement('li');
       ul.appendChild(windEl);
-      windEl.textContent = wind;
+      windEl.textContent = "Wind: " + wind + "km/h";
       
-      return col;
+      return card;
     }
-  }
+
   
   //time input
   
